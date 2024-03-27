eaca86a01ae2dcf991c4b33445b1e1d6f5e40038
Adrián Matušík <57508483+Fudgeee@users.noreply.github.com>
2024-03-27 16:52:04 +0100

    Delete IJA directory

--- IJA/uloha 2/lib/get-libs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-wget -O jackson-annotations-2.9.6.jar https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-annotations/2.9.6/jackson-annotations-2.9.6.jar
-wget -O jackson-core-2.9.6.jar https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-core/2.9.6/jackson-core-2.9.6.jar 
-wget -O jackson-databind-2.9.6.jar https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-databind/2.9.6/jackson-databind-2.9.6.jar
